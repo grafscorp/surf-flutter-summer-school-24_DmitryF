6f9efbb8ef7757216e2e6230ecbf5096d66a636d
grafscorp <golyandringraf@gmail.com>
2024-07-23 10:38:07 +0300

    Revert "Rename func for getImage"
    This reverts commit 1472ed42ea1be3f1de848ac26b0e50541cf13dc8.

--- lib/postgramAPI/postgramAPI.dart
+++ lib/postgramAPI/postgramAPI.dart
@@ -3,20 +3,20 @@ import 'package:flutter/material.dart';
 class PostgramAPI {
   static List<Widget> getAllPhotoFromAccount() {
     return <Widget>[
-      getImageCard(
+      getImageWithPadding(
           "https://s1.1zoom.ru/big3/984/Canada_Parks_Lake_Mountains_Forests_Scenery_Rocky_567540_3840x2400.jpg"),
-      getImageCard(
+      getImageWithPadding(
           "https://i.pinimg.com/736x/68/bb/9f/68bb9fa7c140101cafb7d91044e2d53b.jpg"),
-      getImageCard(
+      getImageWithPadding(
           "https://get.pxhere.com/photo/man-person-girl-woman-camera-photography-dslr-portrait-spring-red-lens-color-autumn-canon-romance-season-taking-photo-photograph-beauty-emotion-photo-shoot-portrait-photography-939435.jpg"),
-      getImageCard(
+      getImageWithPadding(
           "https://get.pxhere.com/photo/person-girl-woman-night-camera-photography-photographer-portrait-red-color-canon-fashion-shooting-singing-photograph-tr-nh-patience-s-kien-flash-light-human-positions-1207048.jpg"),
-      getImageCard(
+      getImageWithPadding(
           "https://get.pxhere.com/photo/person-girl-woman-night-camera-photography-photographer-portrait-red-color-canon-fashion-shooting-singing-photograph-tr-nh-patience-s-kien-flash-light-human-positions-1207048.jpg"),
     ];
   }
 
-  static getImageCard(String photoUrl) => Card(
+  static getImageWithPadding(String photoUrl) => Card(
       semanticContainer: true,
       clipBehavior: Clip.antiAliasWithSaveLayer,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),

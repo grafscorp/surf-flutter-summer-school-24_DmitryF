6bda3f60d6fdd99b550f9764c1a46cf9ccbb42b7
Grafscorp <golyandringraf@gmail.com>
2024-07-23 21:03:05 +0300

    Update Themes

--- lib/themes/themedark.dart
+++ lib/themes/themedark.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:surf_flutter_summer_school_24/themes/scr/textTheme.dart';
+import 'package:surf_flutter_summer_school_24/themes/scr/TextTheme.dart';
 
-ThemeData DarkTheme() {
+ThemeData darkTheme() {
   return ThemeData(
-      textTheme: darkTextStyle(),
-      brightness: Brightness.dark,
-      scaffoldBackgroundColor: Colors.black,
-      // extensions: <ThemeExtension<dynamic>> [
-      // ],
-      appBarTheme: const AppBarTheme(backgroundColor: Colors.black),
-      iconTheme: IconThemeData(color: Colors.white));
+    textTheme: darkTextStyle(),
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: Colors.black,
+    // extensions: <ThemeExtension<dynamic>> [
+    // ],
+    appBarTheme: const AppBarTheme(backgroundColor: Colors.black),
+    iconTheme: const IconThemeData(color: Colors.white),
+    elevatedButtonTheme: const ElevatedButtonThemeData(style: ButtonStyle()),
+  );
 }
--- lib/themes/themelight.dart
+++ lib/themes/themelight.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:surf_flutter_summer_school_24/themes/scr/textTheme.dart';
+import 'package:surf_flutter_summer_school_24/themes/scr/TextTheme.dart';
 
-ThemeData LightTheme() {
+ThemeData lightTheme() {
   return ThemeData(
-      textTheme: lightTextStyle(),
-      brightness: Brightness.light,
-      scaffoldBackgroundColor: Colors.white,
-      // extensions: <ThemeExtension<dynamic>> [
-      // ],
-      appBarTheme: const AppBarTheme(backgroundColor: Colors.white),
-      iconTheme: IconThemeData(color: Colors.black));
+    textTheme: lightTextStyle(),
+    brightness: Brightness.light,
+    scaffoldBackgroundColor: Colors.white,
+    // extensions: <ThemeExtension<dynamic>> [
+    // ],
+    appBarTheme: const AppBarTheme(backgroundColor: Colors.white),
+    iconTheme: const IconThemeData(color: Colors.black),
+    elevatedButtonTheme: const ElevatedButtonThemeData(style: ButtonStyle()),
+  );
 }

34623d87147663196b7e6e717b67041c38d246b3
Grafscorp <golyandringraf@gmail.com>
2024-07-23 21:02:55 +0300

    Start prepared for to connect to server

--- lib/postgramAPI/postgramAPI.dart
+++ lib/postgramAPI/postgramAPI.dart
@@ -1,45 +1,8 @@
-import 'dart:ffi';
+// ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
 
 class PostgramAPI {
-  static List<Widget> getAllPhotoFromAccount() {
-    return <Widget>[
-      getImageCard(
-          "https://s1.1zoom.ru/big3/984/Canada_Parks_Lake_Mountains_Forests_Scenery_Rocky_567540_3840x2400.jpg"),
-      getImageCard(
-          "https://i.pinimg.com/736x/68/bb/9f/68bb9fa7c140101cafb7d91044e2d53b.jpg"),
-      getImageCard(
-          "https://get.pxhere.com/photo/man-person-girl-woman-camera-photography-dslr-portrait-spring-red-lens-color-autumn-canon-romance-season-taking-photo-photograph-beauty-emotion-photo-shoot-portrait-photography-939435.jpg"),
-      getImageCard(
-          "https://get.pxhere.com/photo/person-girl-woman-night-camera-photography-photographer-portrait-red-color-canon-fashion-shooting-singing-photograph-tr-nh-patience-s-kien-flash-light-human-positions-1207048.jpg"),
-      getImageCard(
-          "https://get.pxhere.com/photo/person-girl-woman-night-camera-photography-photographer-portrait-red-color-canon-fashion-shooting-singing-photograph-tr-nh-patience-s-kien-flash-light-human-positions-1207048.jpg"),
-    ];
-  }
-
-  static getImageCard(String photoUrl) => Card(
-      semanticContainer: true,
-      clipBehavior: Clip.antiAliasWithSaveLayer,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-      child: Image.network(
-        photoUrl,
-        fit: BoxFit.cover,
-        loadingBuilder: (context, child, loadingProgress) {
-          if (loadingProgress == null) {
-            return child;
-          } else {
-            return Container(
-              decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                colors: [Colors.black, Colors.white],
-              )),
-            );
-          }
-        },
-      ));
-  //ExtendedImage()
-
   //get image methods 2
 
   static getImagesFromAccount() {
@@ -58,10 +21,11 @@ class PostgramAPI {
     ];
   }
 
-  static Image _getImageNetworkSctrech(String url) {
+  static Image _getImageNetworkSctrech(String url,
+      {BoxFit fit = BoxFit.cover}) {
     return Image.network(
       url,
-      fit: BoxFit.cover,
+      fit: fit,
       loadingBuilder: (context, child, loadingProgress) {
         if (loadingProgress == null) {
           return child;

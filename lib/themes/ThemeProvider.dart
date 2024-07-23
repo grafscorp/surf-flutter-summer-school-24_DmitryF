2f82af8e5ab4e722c0dcff0ffa14d65b834ac2d4
Grafscorp <golyandringraf@gmail.com>
2024-07-23 21:03:20 +0300

    Update ThemeProvider

---
+++ lib/themes/ThemeProvider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Themeprovider extends ChangeNotifier {
+  late SharedPreferences prefTheme;
+
+  bool _isDark = false;
+  bool get isDark => _isDark;
+
+  void changeTheme() {
+    _isDark = !_isDark;
+    prefTheme.setBool("isDark", _isDark);
+    notifyListeners();
+  }
+
+  void init() async {
+    prefTheme = await SharedPreferences.getInstance();
+    _isDark = prefTheme.getBool("isDark") ?? true;
+    notifyListeners();
+  }
+}

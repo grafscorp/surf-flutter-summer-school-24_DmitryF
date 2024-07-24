c43550dfba5350f6fe7184d0e1370f25af993bc7
Grafscorp <golyandringraf@gmail.com>
2024-07-24 19:22:46 +0300

    Disabled scrolledUnderElevation for AppBar

--- lib/themes/Themedark.dart
+++ lib/themes/Themedark.dart
@@ -3,12 +3,18 @@ import 'package:surf_flutter_summer_school_24/themes/scr/TextTheme.dart';
 
 ThemeData darkTheme() {
   return ThemeData(
+    progressIndicatorTheme:
+        const ProgressIndicatorThemeData(color: Colors.white),
+    colorScheme: const ColorScheme.dark(),
+    indicatorColor: Colors.white,
+    primaryColor: Colors.white,
     textTheme: darkTextStyle(),
     brightness: Brightness.dark,
     scaffoldBackgroundColor: Colors.black,
     // extensions: <ThemeExtension<dynamic>> [
     // ],
-    appBarTheme: const AppBarTheme(backgroundColor: Colors.black),
+    appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.black, scrolledUnderElevation: 0.0),
     iconTheme: const IconThemeData(color: Colors.white),
     elevatedButtonTheme: const ElevatedButtonThemeData(style: ButtonStyle()),
   );
--- lib/themes/Themelight.dart
+++ lib/themes/Themelight.dart
@@ -3,12 +3,18 @@ import 'package:surf_flutter_summer_school_24/themes/scr/TextTheme.dart';
 
 ThemeData lightTheme() {
   return ThemeData(
+    progressIndicatorTheme:
+        const ProgressIndicatorThemeData(color: Colors.black),
+    colorScheme: const ColorScheme.light(),
+    indicatorColor: Colors.black,
+    primaryColor: Colors.black,
     textTheme: lightTextStyle(),
     brightness: Brightness.light,
     scaffoldBackgroundColor: Colors.white,
     // extensions: <ThemeExtension<dynamic>> [
     // ],
-    appBarTheme: const AppBarTheme(backgroundColor: Colors.white),
+    appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.white, scrolledUnderElevation: 0.0),
     iconTheme: const IconThemeData(color: Colors.black),
     elevatedButtonTheme: const ElevatedButtonThemeData(style: ButtonStyle()),
   );

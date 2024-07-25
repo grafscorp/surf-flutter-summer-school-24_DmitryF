fc384df6333b305abe2ebfeecde2321873534309
Grafscorp <golyandringraf@gmail.com>
2024-07-25 22:33:00 +0300

    Tried show imageDialog

--- lib/pages/GalleryPage.dart
+++ lib/pages/GalleryPage.dart
@@ -1,7 +1,12 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:surf_flutter_summer_school_24/models/ImageCompletedDialog.dart';
+import 'package:surf_flutter_summer_school_24/models/ImageFailedLoadDialog.dart';
+import 'package:surf_flutter_summer_school_24/models/ImageLoadingDialog.dart';
 import 'package:surf_flutter_summer_school_24/pages/CarouselPage.dart';
+import 'package:surf_flutter_summer_school_24/postgramAPI/bloc/postImage/ImagePostBloc.dart';
 import 'package:surf_flutter_summer_school_24/postgramAPI/models/PhotoData.dart';
 import 'package:surf_flutter_summer_school_24/postgramAPI/models/PostogramImage.dart';
 
@@ -26,24 +31,41 @@ class _GalleryPageState extends State<GalleryPage> {
   ///Override Methods
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          mainAxisSpacing: 3,
-          crossAxisSpacing: 3,
-          crossAxisCount: 3,
-        ),
-        itemCount: listPhoto.length,
-        itemBuilder: (context, index) {
-          return _photoContainer(index);
-        });
+    return BlocBuilder<ImagePostBloc, ImagePostState>(
+      builder: (context, state) {
+        if (state is ImagePostLoaded) {
+          //_imageListBloc.add(LoadImageListEvent());
+
+          //Navigator.pop(context);
+          return const ImageCompletedDialog();
+        } else if (state is ImagePostLoading) {
+          return const ImageLoadingDialog();
+        } else if (state is ImagePostEror) {
+          return const ImageFailedLoadDialog();
+        } else {
+          //_imageListBloc.add(LoadImageListEvent());
+          return GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              mainAxisSpacing: 3,
+              crossAxisSpacing: 3,
+              crossAxisCount: 3,
+            ),
+            itemCount: listPhoto.length,
+            itemBuilder: (context, index) {
+              return _photoContainer(index);
+            },
+          );
+        }
+      },
+    );
   }
 
   Widget _photoContainer(int index) {
     const photoSize = Size(116, 116);
     return SizedBox(
-        height: photoSize.height,
-        width: photoSize.width,
-        child: InkWell(
+      height: photoSize.height,
+      width: photoSize.width,
+      child: InkWell(
           onTap: () {
             Navigator.push(
                 context,
@@ -51,11 +73,9 @@ class _GalleryPageState extends State<GalleryPage> {
                     builder: (context) =>
                         CarouselPage(indexPhoto: index, listPhoto: listPhoto)));
           },
-          child: listPhoto.length > 0
-              ? PostogramImage(
-                  photoData: listPhoto.elementAt(index),
-                )
-              : null,
-        ));
+          child: PostogramImage(
+            photoData: listPhoto.elementAt(index),
+          )),
+    );
   }
 }

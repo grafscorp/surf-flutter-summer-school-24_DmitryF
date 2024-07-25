5d6391d8f9364127253f1a04248c3697721c349c
grafscorp <golyandringraf@gmail.com>
2024-07-25 17:02:54 +0300

    Switch returned type

--- lib/models/ImagePicker.dart
+++ lib/models/ImagePicker.dart
@@ -3,12 +3,12 @@ import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 
 class ImagePickerController {
-  static Future<File?> pickImageFromGallery() async {
+  static Future<XFile?> pickImageFromGallery() async {
     final _tempImage =
         await ImagePicker().pickImage(source: ImageSource.gallery);
     if (_tempImage == null) {
       return null;
     }
-    return File(_tempImage.path);
+    return _tempImage;
   }
 }

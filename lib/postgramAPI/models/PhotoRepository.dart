99c527afcabd36454f236c57da157df1e026c55d
grafscorp <golyandringraf@gmail.com>
2024-07-25 17:04:46 +0300

    switch type argument

--- lib/postgramAPI/models/PhotoRepository.dart
+++ lib/postgramAPI/models/PhotoRepository.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
+import 'package:image_picker/image_picker.dart';
 import 'package:surf_flutter_summer_school_24/postgramAPI/models/PhotoData.dart';
 
 abstract interface class PhotoRepository {
   Future<List<PhotoData>?> getPhotos();
+  Future<bool> postPhoto(XFile newPhoto);
 }

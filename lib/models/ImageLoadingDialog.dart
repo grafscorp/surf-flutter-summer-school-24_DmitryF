5847e74e38db18e118c2319d4b90330b1689bd29
Grafscorp <golyandringraf@gmail.com>
2024-07-25 22:30:42 +0300

    Create Imagge Dialogs

---
+++ lib/models/ImageLoadingDialog.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ImageFailedLoadDialog extends StatelessWidget {
+  const ImageFailedLoadDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const AlertDialog(
+      title: Text("Загрузка изображения..."),
+      content: SizedBox(
+        width: 60,
+        height: 60,
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}

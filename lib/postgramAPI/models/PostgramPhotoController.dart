7e342c482f2a48fa6ec9f22b35b68dd7709bd636
grafscorp <golyandringraf@gmail.com>
2024-07-25 10:13:26 +0300

    fix field postogramController

--- lib/postgramAPI/models/PostgramPhotoController.dart
+++ lib/postgramAPI/models/PostgramPhotoController.dart
@@ -3,9 +3,10 @@ import 'package:surf_flutter_summer_school_24/postgramAPI/models/MockPhotoReposi
 import 'package:surf_flutter_summer_school_24/postgramAPI/models/PhotoData.dart';
 
 class PostgramPhotoController {
-  static List<PhotoData> photoList = [];
+  static List<PhotoData> _photoList = [];
+  static List<PhotoData> get photoList => _photoList;
   static Future<void> updatePhotoList() async {
-    photoList = await MockPhotoRepository().getPhotos();
+    _photoList = await MockPhotoRepository().getPhotos();
     //photoList = await PostgramAPI().getPhotos();
   }
 }
